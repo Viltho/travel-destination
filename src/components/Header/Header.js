53f816e5a37de7e18363e7fb7cde6797fad54eb4
Abdullah Abu Hamad <abdullahabuhammed@gmail.com>
2023-03-07 17:35:55 +0300

    first time magic

---
+++ src/components/Header/Header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Header.css'
+
+function Header() {
+    return (
+        <div
+            className="header">
+            <nav>
+                <h1>This is the header</h1>
+                <img/>
+            </nav>
+        </div>
+    )
+}
+
+
+export default Header
